58dd6b1c1e8b8f953132117b1c8558c7fcb9bdf9
Yogendra <a.yogendra65@gmail.com>
2024-12-09 09:51:46 +0530

    Updated the files in ticket booking system

---
+++ src/main/java/com/ticketbooking/app/TestMain.java
@@ -0,0 +1,36 @@
+package com.ticketbooking.app;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.boot.Metadata;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
+public class TestMain {
+    public static void main(String[] args) {
+        StandardServiceRegistry ssr = new StandardServiceRegistryBuilder().configure("hibernate.cfg.xml").build();
+        Metadata meta = new MetadataSources(ssr).getMetadataBuilder().build();
+
+        SessionFactory factory = meta.getSessionFactoryBuilder().build();
+        Session session = factory.openSession();
+        Transaction t = session.beginTransaction();
+
+        User u1 = new User();
+        u1.setName("Yogi");
+        u1.setPhno("9866709807");
+        u1.setEmail("a.yogendra@65gmail.com");
+        u1.setAddress("Ram nagar market");
+        u1.setUserName("yogi");
+        u1.setPassword("somePassword"); 
+
+        session.save(u1);
+        t.commit();
+        System.out.println("successfully saved");
+        
+        factory.close();
+        session.close();
+    }
+}
+
---
+++ src/main/java/com/ticketbooking/app/User.java
@@ -0,0 +1,148 @@
+package com.ticketbooking.app;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // can use AUTO, SEQUENCE, or TABLE
+    @Column(name = "user_id")
+    private int userId;
+
+    private String name;
+
+    private String phno;
+
+    private String email;
+
+    private String address;
+
+    @Column(name = "user_name")
+    private String userName;
+
+    private String password;
+
+    private String createdBy;
+
+    private String createdDate;
+
+    private String modifiedBy;
+
+    private String modifiedDate;
+
+    public User() {
+        super();
+    }
+
+    public User(int userId, String name, String phno, String email, String address, String userName, String password,
+                String createdBy, String createdDate, String modifiedBy, String modifiedDate) {
+        super();
+        this.userId = userId;
+        this.name = name;
+        this.phno = phno;
+        this.email = email;
+        this.address = address;
+        this.userName = userName;
+        this.password = password;
+        this.createdBy = createdBy;
+        this.createdDate = createdDate;
+        this.modifiedBy = modifiedBy;
+        this.modifiedDate = modifiedDate;
+    }
+
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhno() {
+        return phno;
+    }
+
+    public void setPhno(String phno) {
+        this.phno = phno;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getCreatedBy() {
+        return createdBy;
+    }
+
+    public void setCreatedBy(String createdBy) {
+        this.createdBy = createdBy;
+    }
+
+    public String getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(String createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public String getModifiedBy() {
+        return modifiedBy;
+    }
+
+    public void setModifiedBy(String modifiedBy) {
+        this.modifiedBy = modifiedBy;
+    }
+
+    public String getModifiedDate() {
+        return modifiedDate;
+    }
+
+    public void setModifiedDate(String modifiedDate) {
+        this.modifiedDate = modifiedDate;
+    }
+}
